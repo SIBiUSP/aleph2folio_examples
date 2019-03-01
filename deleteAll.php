eea0c859fc769548c1a14c6dc1d60be1b3fab413
Tiago <tiago@bdpife2.sibi.usp.br>
2019-03-01 12:01:41 -0300

    Initial

--- deleteAll.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-
-require 'inc/functions.php';
-
-$folioCookies = FolioREST::loginREST();
-//FolioREST::deleteItensREST($folioCookies);
-FolioREST::deleteAllRecordsREST($folioCookies);
-?>
