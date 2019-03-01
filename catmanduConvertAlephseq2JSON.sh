5545b17b18cdb7e6d7b849821db9157cfadf949c
Tiago <tiago@bdpife2.sibi.usp.br>
2019-03-01 08:24:27 -0300

    Initial

--- catmanduConvertAlephseq2JSON.sh
+++ catmanduConvertAlephseq2JSON.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-catmandu convert MARC --type ALEPHSEQ to JSON --line_delimited 1 < input/2records.seq --fix fixesCatmandu.txt | jq .
+catmandu convert MARC --type ALEPHSEQ to JSON < input/2records.seq --fix fixesCatmandu.txt | jq .
