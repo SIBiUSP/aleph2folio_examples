a6a9b40c072ed75f97ee75bb41fec86869338b24
Tiago <tiago@bdpife2.sibi.usp.br>
2019-03-01 14:43:42 -0300

    Initial

--- catmanduConvertAlephseq2JSON.sh
+++ catmanduConvertAlephseq2JSON.sh
@@ -11,15 +11,24 @@ FOLIO_TOKEN=$( curl --silent --output /dev/null -w '\n' -D - -X POST -H "Content
 
 # Convert ALEPHSEQ to Folio Codex JSON
 
-catmandu convert MARC --type ALEPHSEQ to JSON < input/2records.seq --fix fixesCatmandu.txt  > output/output.json
+catmandu convert MARC --type ALEPHSEQ to JSON < input/bas01iri.seq --fix fixesCatmandu.txt  >> output/output.json
 
 # Import Folio Codex on JSON
+jq -c '.[]' output/output.json >| output/line.txt 
 
-jq -c '.[]' output/output.json | while read i; do
-    UUID=$(cat /proc/sys/kernel/random/uuid)
-    COMPLETE_JSON=$(jq '. += {"id": "'$UUID'"}')
-    curl -H "Accept: application/json" -H "Content-type: application/json" -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $FOLIO_TOKEN" -X POST -d "$COMPLETE_JSON"  http://172.31.1.52:9130/inventory/instances
-done
+while read -r line; do 
+
+  echo $line > input/temp.json
+
+  UUID=$(uuidgen -r)
+  jq -c '. += {"id": "'$UUID'"}' < input/temp.json > output/complete.json
+  curl -H "Accept: application/json" -H "Content-type: application/json" -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $FOLIO_TOKEN" -X POST -d @output/complete.json  http://172.31.1.52:9130/inventory/instances
+  #unset UUID
+  rm input/temp.json
+  rm output/complete.json
+
+done < output/line.txt
 
 # Delete temp file
 rm output/output.json
+rm output/line.txt
