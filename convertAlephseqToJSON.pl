047ea7114e67106172e961286cbcab5765d461eb
Tiago <tiago@bdpife2.sibi.usp.br>
2019-03-14 12:53:02 -0300

    Initial

--- convertAlephseqToJSON.pl
+++ convertAlephseqToJSON.pl
@@ -6,7 +6,7 @@ use Catmandu;
 use strict;
 use warnings;
 
-my $importer = Catmandu->importer('MARC',file => 'input/bas01iri.seq' , type => 'ALEPHSEQ');
+my $importer = Catmandu->importer('MARC',file => 'input/bas01.seq' , type => 'ALEPHSEQ');
 my $fixer    = Catmandu->fixer('fixesCatmandu.txt');
 #my $exporter = Catmandu->exporter('JSON', array => 0, line_delimited =>1, file => "output/output1.json");
 
@@ -16,6 +16,10 @@ $fixer->fix($importer->benchmark)->each(sub {
   my $rec = $_[0];
   Catmandu->exporter('JSON', array => 0, file => "sample-data/instance-storage/instances/out_${count}.json")->add($rec);
   $count++;
+  if(($count % 1000) == 0){
+    system('perl load-data.pl --custom-method "instances/"=PUT sample-data');
+    unlink glob "'sample-data/instance-storage/instances/*.*'";
+  }
 });
 
 system('perl load-data.pl --custom-method "instances/"=PUT sample-data');
