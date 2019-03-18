4698e98ef91588243323af09fa7bd23afa4ccf6c
Tiago <tiago@bdpife2.sibi.usp.br>
2019-03-18 16:29:49 -0300

    Initial

---
+++ convertAlephseqToMARC-In-JSON.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+# Convert ALEPHSEQ to JSON
+
+use Catmandu;
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $importer = Catmandu->importer('MARC',file => $ARGV[0], type => 'ALEPHSEQ');
+my $fixer    = Catmandu->fixer('marcInJSON.txt');
+
+my $count = 0;
+
+$importer->benchmark->each(sub {
+   my $rec = $_[0];
+   Catmandu->exporter('MARC', type => 'MiJ', array => 0, fix=> 'marcInJSON.txt', file => "outputJSON/out_${count}.json")->add($rec);
+   $count++;
+   if(($count % 100) == 0){
+      system('/bin/bash importSourceRecord.sh');
+   }
+});
+
--- importSourceRecord.sh
+++ importSourceRecord.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # Import Record Source to FOLIO
 
-catmandu convert MARC --type ALEPHSEQ to MARC --type MiJ --array 0 --line_delimited 1 < $1 > tmp/output.json
+#perl convertAlephseqToMARC-In-JSON.pl $1
 
-split -l 1 -d --additional-suffix=.json tmp/output.json outputJSON/source_file
-
-
-rm tmp/output.json
+counter=0
 
 for entry in "outputJSON"/*
 do  
   SYSNO=$(cat $entry | jq '.fields[]."001"' | grep '"' | tr -d '"') 
-  UUID=$(cat output/sysno_id.csv | grep $SYSNO | cut -d, -f2)
+  UUID=$(LC_ALL=C fgrep "$SYSNO" < output/sysno_id.csv | cut -d, -f2)
   mkdir -p sample-data/instance-storage/instances/$UUID/source-record/marc-json
   mv $entry sample-data/instance-storage/instances/$UUID/source-record/marc-json/$UUID.json
+  counter=$((counter+1))
+  if [[ $(( counter % 10 )) = 0 ]]
+  then
+    perl load-data.pl --custom-method "instances/"=PUT sample-data
+    rm -rf sample-data/instance-storage/instances/*
+  fi
 done
 
 perl load-data.pl --custom-method "instances/"=PUT sample-data
