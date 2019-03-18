683e89f1451a52c955a83dad8f8ac0fc80cebb23
Tiago <tiago@bdpife2.sibi.usp.br>
2019-03-18 13:07:37 -0300

    Initial

--- convertAlephseqToJSON.pl
+++ convertAlephseqToJSON.pl
@@ -6,7 +6,7 @@ use Catmandu;
 use strict;
 use warnings;
 
-my $importer = Catmandu->importer('MARC',file => 'input/2records.seq' , type => 'ALEPHSEQ');
+my $importer = Catmandu->importer('MARC',file => $ARGV[0] , type => 'ALEPHSEQ');
 my $fixer    = Catmandu->fixer('fixesCatmandu.txt');
 #my $exporter = Catmandu->exporter('JSON', array => 0, line_delimited =>1, file => "output/output1.json");
 
