1288e11ce05149c9c50985713c572786dbb3e778
Tiago <tiago@bdpife2.sibi.usp.br>
2019-03-27 10:46:03 -0300

    Initial

--- testPHP.php
+++ testPHP.php
@@ -28,15 +28,18 @@ while ($line = fgets(STDIN)) {
         $body = fixes($marc);
         $jsonOutput = json_encode($body); 
 
-        FolioREST::addRecordREST($folioCookies, $jsonOutput);
+        $instanceFile = fopen("sample-data/instance-storage/instances/$i.json", "w") or die("Unable to open file!");
+        fwrite($instanceFile, $jsonOutput);
+        fclose($instanceFile);
         
         # Import Source Record
 
-        shell_exec('catmandu convert MARC --type ALEPHSEQ to MARC --type MiJ --array 0 --line_delimited 1 < tmp/alephseq.seq > tmp/source.json');
         $instanceID = $body["id"];
-        shell_exec("curl -s -S -w -D -X PUT -H 'Content-type: application/json' -H 'X-Okapi-Tenant: diku' -H 'X-Okapi-Token: $folioCookies' -d @tmp/source.json $OKAPI/instance-storage/instances/$instanceID/source-record/marc-json > /dev/null");
+        mkdir("sample-data/instance-storage/instances/$instanceID/source-record/marc-json", 0755, true);        
+        shell_exec("catmandu convert MARC --type ALEPHSEQ to MARC --type MiJ --array 0 --line_delimited 1 < tmp/alephseq.seq > sample-data/instance-storage/instances/$instanceID/source-record/marc-json/source.json");
+        #shell_exec("curl -s -S -w -D -X PUT -H 'Content-type: application/json' -H 'X-Okapi-Tenant: diku' -H 'X-Okapi-Token: $folioCookies' -d @tmp/source.json $OKAPI/instance-storage/instances/$instanceID/source-record/marc-json > /dev/null");
         unlink("tmp/alephseq.seq");
-        unlink("tmp/source.json");               
+        #unlink("tmp/source.json");               
 
         if (isset($marc["record"]["Z30"])) {
             # Create holdings 
@@ -46,17 +49,23 @@ while ($line = fgets(STDIN)) {
             }
             $librariesArrayUnique = array_unique($librariesArray);
 
+            $i_holdings = 0;
             foreach ($librariesArrayUnique as $libraryCode) {
                 $holdings["id"] = gen_uuid();
                 $holdings["instanceId"] = $body["id"];
                 $holdings["permanentLocationId"] = decode::get_library_location_id($libraryCode);
                 $jsonHoldings = json_encode($holdings);
-                $holdingsID["$libraryCode"] = $holdings["id"]; 
-                FolioREST::addHoldingsREST($folioCookies, $jsonHoldings);  
+                $holdingsID["$libraryCode"] = $holdings["id"];
+                
+                $holdingsFile = fopen("sample-data/holdings-storage/holdings/$i_holdings.json", "w") or die("Unable to open file!");
+                fwrite($holdingsFile, $jsonHoldings);
+                fclose($holdingsFile);
+                $i_holdings++;
             }
          
 
-            # Create itens    
+            # Create itens
+            $i_itens = 0;    
             foreach ($marc["record"]["Z30"] as $item) {
 
                 $libraryCodeitem = $item["1"];
@@ -68,7 +77,12 @@ while ($line = fgets(STDIN)) {
                 $itemArray["permanentLoanTypeId"] = "2b94c631-fca9-4892-a730-03ee529ffe27";
                 
                 $jsonItem = json_encode($itemArray);
-                FolioREST::addItemREST($folioCookies, $jsonItem); 
+
+                $itemFile = fopen("sample-data/item-storage/items/$i_itens.json", "w") or die("Unable to open file!");
+                fwrite($itemFile, $jsonItem);
+                fclose($itemFile);
+
+                $i_itens++;
 
             }
         }
@@ -83,3 +97,11 @@ while ($line = fgets(STDIN)) {
     $sysno_old = $sysno;
     $i++;
 }
+
+shell_exec('perl load-data.pl --custom-method "instances/"=PUT sample-data');
+
+array_map('unlink', glob("sample-data/instance-storage/instances/*.json"));
+array_map('unlink', glob("sample-data/holdings-storage/holdings/*.json"));
+array_map('unlink', glob("sample-data/item-storage/items/*.json"));
+
+shell_exec('rm -rf sample-data/instance-storage/instances/*');
