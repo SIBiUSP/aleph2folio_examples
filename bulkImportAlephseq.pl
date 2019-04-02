a54f20c02926dd42b3d9f388825bb7a2ab06315d
jan <janleduc@usp.br>
2019-04-02 17:17:53 -0300

    minor improvements / code readability

--- bulkImportAlephseq.pl
+++ bulkImportAlephseq.pl
@@ -1,7 +1,7 @@
 
 # perl version of bulkImportAlephseq.php
 # sample usage:
-# perl bulkImportAlephseq.pl fixesBias.txt < input/2records.seq ; ./deleteAll.sh
+# perl bulkImportAlephseq.pl fixesBias.txt < input/2records.seq
 #
 
 use Data::Walk;
@@ -21,6 +21,9 @@ use warnings;
 
 my $ua = LWP::UserAgent->new;
 
+my $req;
+my $res;
+
 # debug_ua($ua);
 $ua->agent("MyApp/0.1");
 
@@ -29,7 +32,7 @@ my $tenant = 'diku';
 my $password = 'admin';
 my $username = 'diku_admin';
 
-my $login_req = HTTP::Request->new(
+$req = HTTP::Request->new(
  'POST',
  "$okapiurl/authn/login",
  [ 'X-Okapi-Tenant' => $tenant,
@@ -38,14 +41,12 @@ my $login_req = HTTP::Request->new(
  encode_utf8(encode_json({ 'username' => $username, 'password' => $password }))
 );
 
-my $login_res = $ua->request($login_req);
+$res = $ua->request($req);
+die "error: ".$res->status_line."\n" unless $res->is_success;
 
-die "error: ".$login_res->status_line."\n" unless $login_res->is_success;
+my $token = $res->header('X-Okapi-Token');
 
-my $token = $login_res->header('X-Okapi-Token');
-
-
-my $get_req = HTTP::Request->new(
+$req = HTTP::Request->new(
   'GET',
   "$okapiurl/locations?limit=2000",
   ['X-Okapi-Tenant' => $tenant,
@@ -54,15 +55,15 @@ my $get_req = HTTP::Request->new(
   'X-Okapi-Token' => $token ]
 );
 
-my $get_resp = $ua->request($get_req);
-die "FAILED loading " . $get_resp->status_line . ": " . $get_resp->content . "\n" unless $get_resp->is_success;
+$res = $ua->request($req);
+die "error: " . $res->status_line . ": " . $res->content . "\n" unless $res->is_success;
 
-my $locations = decode_json($get_resp->content);
+my $locations = decode_json($res->content);
 
 my $holdings = {};
 
 my $importer = Catmandu->importer('MARC', 'type' => 'ALEPHSEQ');
-my $fixer = Catmandu->fixer($ARGV[0]);
+my $seqfix = Catmandu->fixer($ARGV[0]);
 my $mijfix = Catmandu->fixer('remove_field(_id)','marc_remove(FMT)','marc_in_json()');
 
 $importer->each( sub {
@@ -71,11 +72,11 @@ $importer->each( sub {
 
   my $mijd = dclone $data;
 
-  $fixer->fix($data);
+  $seqfix->fix($data);
 
   my $instanceID = $data->{'id'};
 
-  my $post_req = HTTP::Request->new(
+  $req = HTTP::Request->new(
     'POST',
     "$okapiurl/instance-storage/instances",
     ['X-Okapi-Tenant' => $tenant,
@@ -85,12 +86,12 @@ $importer->each( sub {
     encode_json($data)
   );
 
-  my $post_resp = $ua->request($post_req);
-  die "FAILED loading " . $post_resp->status_line . ": " . $post_resp->content . "\n" unless $post_resp->is_success;
+  $res = $ua->request($req);
+  die "error: " . $res->status_line . ": " . $res->content . "\n" unless $res->is_success;
 
   $mijfix->fix($mijd);
 
-  my $put_req = HTTP::Request->new(
+  $req = HTTP::Request->new(
     'PUT',
     "$okapiurl/instance-storage/instances/$instanceID/source-record/marc-json",
     ['X-Okapi-Tenant' => $tenant,
@@ -100,8 +101,8 @@ $importer->each( sub {
     encode_utf8(encode_json($mijd))
   );
 
-  my $put_resp = $ua->request($put_req);
-  die "FAILED loading " . $put_resp->status_line . ": " . $put_resp->content . "\n" unless $put_resp->is_success;
+  $res = $ua->request($req);
+  die "error: " . $res->status_line . ": " . $res->content . "\n" unless $res->is_success;
 
   foreach ( map {exists($_->{'Z30'}) ? $_->{'Z30'} : ()} @{$mijd->{'fields'}} ) {
     foreach ( @{$_->{'subfields'}} ){
@@ -111,7 +112,7 @@ $importer->each( sub {
         $holdings->{$bibcode.'-'.$instanceID}->{'instanceId'} = $instanceID;
         $holdings->{$bibcode.'-'.$instanceID}->{'permanentLocationId'} = (map { $_->{'code'} eq $bibcode ? $_->{'id'} : () } @{$locations->{'locations'}})[0];
 
-        my $holdings_req = HTTP::Request->new(
+        $req = HTTP::Request->new(
           'PUT',
           "$okapiurl/holdings-storage/holdings/".$holdings->{$bibcode.'-'.$instanceID}->{'id'},
           ['X-Okapi-Tenant' => $tenant,
@@ -120,12 +121,13 @@ $importer->each( sub {
           'X-Okapi-Token' => $token ],
           encode_utf8(encode_json($holdings->{$bibcode.'-'.$instanceID}))
         );
-        my $holdings_resp = $ua->request($holdings_req);
-        die "FAILED loading " . $holdings_resp->status_line . ": " . $holdings_resp->content . "\n" unless $holdings_resp->is_success;
-
+        $res = $ua->request($req);
+        die "error: " . $res->status_line . ": " . $res->content . "\n" unless $res->is_success;
       }
     }
+
     my $item = {};
+    
     $item->{'id'} = Data::GUID->new->as_string;
     $item->{'materialTypeId'} = '1a54b431-2e4f-452d-9cae-9cee66c9a892';
     $item->{'permanentLoanTypeId'} = '2b94c631-fca9-4892-a730-03ee529ffe27';
@@ -140,9 +142,8 @@ $importer->each( sub {
         $item->{'barcode'} = $_->{'5'};
       }
     }
-    # print "item json:[\n", encode_json($item), "\n]\n";
 
-    my $item_req = HTTP::Request->new(
+    $req = HTTP::Request->new(
       'PUT',
       "$okapiurl/item-storage/items/".$item->{'id'},
       ['X-Okapi-Tenant' => $tenant,
@@ -151,15 +152,11 @@ $importer->each( sub {
       'X-Okapi-Token' => $token ],
       encode_utf8(encode_json($item))
     );
-    my $item_resp = $ua->request($item_req);
-    die "FAILED loading " . $item_resp->status_line . ": " . $item_resp->content . "\n" unless $item_resp->is_success;
+    $res = $ua->request($req);
+    die "error: " . $res->status_line . ": " . $res->content . "\n" unless $res->is_success;
 
     undef $item;
+    
   }
 
 });
-
-# print " holdings json:[\n", encode_json($holdings), "\n]\n";
-
-# dump($locations);
-
